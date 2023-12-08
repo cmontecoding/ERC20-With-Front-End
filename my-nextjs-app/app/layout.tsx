2dc6ff3b275236b893404695fabfe232e0d7a85c
Arman <armanbabazadeh61@gmail.com>
2023-12-08 16:41:01 -0500

    cooked code

--- my-nextjs-app/app/layout.tsx
+++ my-nextjs-app/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Navbar from "@components/*";
 
 export const metadata = {
   title: "Crypto",
@@ -13,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Navbar></Navbar>
+        {children}
+      </body>
     </html>
   );
 }
