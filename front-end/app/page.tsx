ecf5c7cd144bfeb8ce13e22a170f8d3c09d4703c
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-12-09 13:08:57 -0500

    nuke

--- front-end/app/page.tsx
+++
@@ -1,4 +0,0 @@
-export default function Home() {
-  return <h1 className="font-bold, text-teal-500, text-2x1">Hello World</h1>;
-  <h1>Arman/Andrew</h1>;
-}
--- my-nextjs-app/components/Navbar.tsx
+++
@@ -1,5 +0,0 @@
-const Navbar = () => {
-  return <div className="border-2 border-red-500">Navbar</div>;
-};
-
-export default Navbar;
