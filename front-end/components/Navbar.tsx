1f3a1cafb1879b3a26192eb0b3281929e1f9b57e
Arman <armanbabazadeh61@gmail.com>
2023-12-09 17:21:29 -0500

    Created Navbar/Login Button

--- front-end/components/Navbar.tsx
+++ front-end/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { NAV_LINKS } from "@/constants";
 import Image from "next/image"; //Optimizes images for faster load times
 import Link from "next/link";
+import Button from "./Button"; // Imports from Button componenet
 
 const Navbar = () => {
   return (
@@ -8,18 +9,34 @@ const Navbar = () => {
       <Link href="/">
         <Image src="/logo.png" alt="logo" width={75} height={75}></Image>
       </Link>
-
       <ul className="hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
           <Link
             href={link.href}
             key={link.key}
-            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover: font-bold"
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
           >
             {link.label}
           </Link>
         ))}
       </ul>
+
+      <div className="lg:flexCenter hidden">
+        <Button
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
+
+      <Image
+        src="menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+      />
     </nav>
   );
 };
