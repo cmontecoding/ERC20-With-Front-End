504ab2a4d0268cdd9fd79e0f02c8aa3eecb9d033
Arman <armanbabazadeh61@gmail.com>
2023-12-09 14:08:37 -0500

    Added linked components

--- front-end/components/Navbar.tsx
+++ front-end/components/Navbar.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { NAV_LINKS } from "@/constants";
+import { link } from "fs";
+import Image from "next/image"; //Optimizes images for faster load times
+import Link from "next/link";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/logo.png" alt="logo" width={75} height={75}></Image>
+
+        <ul className="hidden h-full gap-12 lg:flex">
+          {NAV_LINKS.map((link) => (
+            <link href={link.href} key={link.key}>
+              {link.label}
+            </link>
+          ))}
+        </ul>
+      </Link>
+    </nav>
+  );
 };
 
 export default Navbar;
