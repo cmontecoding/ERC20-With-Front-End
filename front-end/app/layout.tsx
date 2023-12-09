17e042bf9e922b88fb7389df86c17d0ba0bb2e42
Arman <armanbabazadeh61@gmail.com>
2023-12-09 13:35:53 -0500

    fixed all issues

--- front-end/app/layout.tsx
+++ front-end/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar></Navbar>
-        children
+        <main className="relative overflow-hidden">{children}</main>
         <Footer></Footer>
       </body>
     </html>
