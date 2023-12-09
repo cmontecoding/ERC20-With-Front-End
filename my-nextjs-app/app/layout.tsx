324415aab842520f4dba5051e91fef986ed93bc7
Arman <armanbabazadeh61@gmail.com>
2023-12-09 12:36:24 -0500

    fixed code

--- my-nextjs-app/app/layout.tsx
+++ my-nextjs-app/app/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from "@components/Navbar";
 import Footer from "@components/Footer";
 //import Navbar from "../components/Navbar";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Crypto",
   description: "next generation of exchnages ",
 };
