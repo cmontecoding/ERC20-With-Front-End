09def0e8d41ee63fa96e23ed068ce57d56bbf69e
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2023-12-21 23:35:36 -0500

    🚀 deploy LendingToken

--- script/LendingToken.s.sol
+++ script/LendingToken.s.sol
@@ -1,12 +1,36 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.21;
 
-import {Script, console2} from "forge-std/Script.sol";
+import {Script} from "../lib/forge-std/src/Script.sol";
+import {LendingToken} from "../src/LendingToken.sol";
+
+/**
+ *
+ * TESTNET DEPLOYMENT: Ethereum Goerli
+ *
+ */
 
 contract LendingTokenScript is Script {
-    function setUp() public {}
+    // contract(s) being deployed
+    LendingToken token;
 
     function run() public {
-        vm.broadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        // deploy contract
+        token = new LendingToken();
+
+        vm.stopBroadcast();
     }
 }
+
+/**
+ * TO DEPLOY:
+ *
+ * To load the variables in the .env file
+ * > source .env
+ *
+ * To deploy and verify our contract
+ * > forge script script/LendingToken.s.sol:LendingTokenScript --rpc-url $ETHEREUM_GOERLI_RPC_URL --broadcast -vvvv
+ */
